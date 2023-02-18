700243be3a4d2ad80324b77dcc38d98a7976e14b
Ryan Jacobo <ryanjacobo@yahoo.com>
2023-02-17 22:16:13 -0500

    first commit

---
+++ src/main/java/com/kipsterworks/springboot/tutorial/service/DepartmentService.java
@@ -0,0 +1,19 @@
+package com.kipsterworks.springboot.tutorial.service;
+
+import com.kipsterworks.springboot.tutorial.entity.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    public Department fetchDepartmentByName(String departmentName);
+}
